7e8fd213ee33bfadd5dec95354d7908a7e26005c
Ian <ianwgunter@gmail.com>
2026-06-05 11:29:49 +0100

    Add middleware to redirect users to registration if no users exist and update auth routes for login and registration based on user count

---
+++ app/Http/Middleware/RedirectIfNoUsers.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
+
+class RedirectIfNoUsers
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->routeIs('register', 'password.*', 'verification.*')) {
+            return $next($request);
+        }
+
+        try {
+            if (Schema::hasTable('users') && User::count() === 0) {
+                return redirect()->route('register');
+            }
+        } catch (\Throwable $e) {
+            //
+        }
+
+        return $next($request);
+    }
+}
