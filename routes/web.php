5950af741e7d31c8292e83ff557fdfc0feab66b0
Ian <ianwgunter@gmail.com>
2026-06-05 11:25:30 +0100

    Refactor authentication routes to improve clarity and maintainability

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,18 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 
-$enableRegistration = false;
-
 try {
-    $enableRegistration = Schema::hasTable('users') ? User::count() === 0 : true;
+    $enableLogin        = Schema::hasTable('users')  ?User::count() > 0 : false;
+    $enableRegistration = Schema::hasTable('users')  ?User::count() === 0 : true;
 } catch (\Throwable $e) {
+    $enableLogin        = true;
     $enableRegistration = false;
 }
 
-Auth::routes(['register' => $enableRegistration]);
+Auth::routes([
+    'register' => $enableRegistration,
+    'login'    => $enableLogin,
+]);
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/home', [HomeController::class, 'index']);
